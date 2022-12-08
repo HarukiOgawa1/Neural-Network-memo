1a04a31a733010f071d8df49688f770d5b4a8883
HarukiOgawa1 <s20c1028uk@s.chibakoudai.jp>
2022-12-08 14:42:50 +0900

    3 layer neural network

--- network03.py
+++ network03.py
@@ -27,7 +27,32 @@ class neuralNetwork:
         pass
 		
     #ニューラルネットワークの学習
-    def train():
+    def train(self, inputs_list, targets_list):
+        #入力リストを行列に変換
+        inputs = numpy.array(inputs_list, ndmin = 2).T
+        targets = numpy.array(targets_list, ndmin = 2).T
+
+        #隠れ層に入ってくる信号の計算
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        #隠れ層で結合された信号を活性化関数により出力
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        #出力層に入ってくる信号の計算
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        #出力層で結合された信号を活性化関数により出力
+        final_outputs = self.activation_function(final_inputs)
+
+        #出力層の誤差　＝　（目標出力ー最終出力）
+        output_errors = targets - final_outputs
+        #隠れ層の誤差は出力層の誤差をリンクの重みの割合で分配
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        #隠れ層と出力層の間のリンクの重みを更新
+        self.who += self.lr * numpy.dot((outputs_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(hidden_outputs))
+
+        #入力層と隠れ層の間のリンクの重みを更新
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)), numpy.transpose(inputs))
+
         pass
 		
     #ニューラルネットワークへの照会
